8442d2b6b51d190bb3fbdea73175b7cac3e77a2a
RayhanalKavey <rayhanalkavey@gmail.com>
2022-10-11 22:19:53 +0600

    Style home header and the topic card.

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -2,22 +2,26 @@ import React, { useContext } from "react";
 import { TopicsContext } from "../../outlet/Main";
 import Topic from "../Topic/Topic";
 import homeImg from "../../media/image/header-image.jpg";
-
+import "./Home.css";
 const Home = () => {
   const topics = useContext(TopicsContext);
   // console.log(topics);
   return (
-    <div className="container mx-auto py-10">
-      <div className="home-header grid grid-cols-4">
+    <div className="container mx-auto my-10">
+      {/*Home Header  */}
+      <div
+        // style={{ backgroundImage: `url('${}')` }}
+        className="home-header grid grid-cols-4 shadow-xl rounded-lg p-2 mb-10"
+      >
         <div className="col-span-1">
           <img className="w-[100%] rounded-lg	" src={homeImg} alt="" />
         </div>
-        <div className="col-span-3 p-5">
+        <div className="col-span-3 p-5 ">
           <h2 className="text-2xl font-bold">
             Most Popular Web Development Interview Questions and Answers for
             2022
           </h2>
-          <p>
+          <p className="mt-5">
             Web Development is quite the buzzword in the industry these days. As
             of now, Web Development is the most popular technology that more and
             more companies need, and if you are preparing for a job interview,
@@ -28,7 +32,8 @@ const Home = () => {
           </p>
         </div>
       </div>
-      <div className="">
+      {/* Topics Card */}
+      <div className="grid grid-cols-2">
         {topics.map((topic) => (
           <Topic topic={topic} key={topic.id}></Topic>
         ))}
--- src/component/Topic/Topic.js
+++ src/component/Topic/Topic.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./Topic.css";
 const Topic = ({ topic }) => {
   // console.log(topic);
   const { id, name, logo, total } = topic;
   return (
-    <div className="">
-      <p>topic ID: {id}</p>
-      <p>Name: {name}</p>
-      <p>Logo: {logo}</p>
-      <p>Total: {total}</p>
-      <Link to={`/topic/${id}`}>
-        <button>Start Practice</button>
-      </Link>
+    <div className="topic-card  items-center col-span-1 rounded-lg bg-[#ECD6BF] p-3 grid grid-cols-2 m-5">
+      <div className="">
+        <p className="text-5xl mb-5 font-semibold">{name}</p>
+        {/* <p>topic ID: {id}</p> */}
+
+        <p>Number of Quiz: {total}</p>
+        <Link
+          className="px-4 py-2 mt-5 inline-block rounded-lg"
+          to={`/topic/${id}`}
+        >
+          Check Skills
+        </Link>
+      </div>
+
+      <img className="w-full 	" src={logo} alt="topic related logo" />
     </div>
   );
 };
