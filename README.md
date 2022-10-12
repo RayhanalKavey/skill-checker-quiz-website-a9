b75a8b65047439c4a301df116f34f9dec6be0dc5
RayhanalKavey <rayhanalkavey@gmail.com>
2022-10-12 11:38:57 +0600

    Make navbar responsive with bar menu.

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 ## This is a skill checker site for web developers.
 
----
-
 ### Description
 
 This is the best interview questions tutorial for someone preparing for a job interview.
@@ -11,8 +9,6 @@ This is the best interview questions tutorial for someone preparing for a job in
 - Here you can check your skills on different web development-related topics like React, JavaScript, etc.
 - Some critical topics are discussed in the blog section. You can check how many questions you answered correctly and see the correct answer.
 - Total questions per topic are shown as a chart in the statistics section.
-
-###
 
 ---
 
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Bars4Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
 import "./Header.css";
 const Header = () => {
+  const [tog, setTog] = useState(false);
+  const toggleMenu = () => {
+    setTog(!tog);
+    console.log("clicked");
+  };
   return (
-    <nav className="nav-bar flex text-xl justify-between items-center px-10 h-[5rem] shadow-xl">
-      <NavLink to="/">Skill Checker</NavLink>
-      <div>
-        <NavLink
-          className={({ isActive }) => (isActive ? "active-link" : undefined)}
-          to="/home"
-        >
-          Home
-        </NavLink>
+    <nav className="nav-bar flex justify-between px-8 items-center h-[6.5rem] text-xl   shadow-xl relative">
+      <NavLink to="/">
+        <span className="sm:text-3xl">Skill Checker</span>
+      </NavLink>
+      <ul
+        className={`lg:flex absolute lg:static ${
+          tog
+            ? "right-0 top-[104px] bg-[#ad7b43] duration-500 ease-in py-7 text-center"
+            : "right-0 top-[-110px]"
+        }`}
+      >
+        <li className={tog ? "mb-5" : undefined}>
+          {" "}
+          <NavLink
+            className={({ isActive }) => (isActive ? "active-link" : undefined)}
+            to="/home"
+          >
+            Home
+          </NavLink>
+        </li>
 
-        <NavLink
-          className={({ isActive }) => (isActive ? "active-link" : undefined)}
-          to="/statistics"
-        >
-          Statistics
-        </NavLink>
+        <li className={tog ? "mb-5" : undefined}>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active-link" : undefined)}
+            to="/statistics"
+          >
+            Statistics
+          </NavLink>
+        </li>
 
-        <NavLink
-          className={({ isActive }) => (isActive ? "active-link" : undefined)}
-          to="/blog"
-        >
-          Blog
-        </NavLink>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active-link" : undefined)}
+            to="/blog"
+          >
+            Blog
+          </NavLink>
+        </li>
+      </ul>
+      <div onClick={toggleMenu} className=" lg:hidden h-6 w-6 text-[#f8f5e7]">
+        {tog ? <XMarkIcon /> : <Bars4Icon />}
       </div>
     </nav>
   );
